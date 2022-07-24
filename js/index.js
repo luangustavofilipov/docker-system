7e97a3a7186de3ed0b04c5c398db15762c6b9fd1
Luan Gustavo Altruda Filipov <luan.gustavo.filipov@gmail.com>
2022-07-24 15:42:28 -0300

    Added bootstrap files

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { dirname } = require("path");
+
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+app.listen(PORT, HOST);
+console.log("Server hosted at http://localhost", +PORT);
